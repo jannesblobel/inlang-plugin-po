c28cdfe37e8895712b76bd017af9927587857848
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-26 14:31:56 +0100

    .

--- inlang.config.js
+++ inlang.config.js
@@ -8,7 +8,7 @@ export async function defineConfig(env) {
   const pluginConfig = {
     // language mean the name of you file
     pathPattern: "./example/{language}.po",
-    referenceResourcePath: "./example/en.pot",
+    referenceResourcePath: null,
   };
 
   return {
