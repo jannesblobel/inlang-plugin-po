860bc9abb30960ad3da2415473fcabfb567acdb3
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-02-23 21:53:08 +0100

    update config

--- inlang.config.js
+++ inlang.config.js
@@ -6,21 +6,23 @@ export async function defineConfig(env) {
   const plugin = await env.$import("../dist/index.js");
 
   const pluginConfig = {
-    pathPattern: "./example/locale/{language}/LC_MESSAGES/django.po",
+    // pathPattern: "./example/locale/{language}/LC_MESSAGES/django.po",
+    pathPattern: "./example/{language}.po",
 
     // Your referenceResourcePath could be
     // null or "./example/locale/en/LC_MESSAGES/django.pot",
     // dependent if you use pot file as you referenceLanguage
-    referenceResourcePath: null,
+    referenceResourcePath: "./example/en.pot",
   };
 
   return {
     referenceLanguage: "en",
-    languages: await plugin.getLanguages({
-      referenceLanguage: "en",
-      ...env,
-      pluginConfig,
-    }),
+    // languages: await plugin.getLanguages({
+    //   referenceLanguage: "en",
+    //   ...env,
+    //   pluginConfig,
+    // }),
+    languages: ["de", "es", "fr", "en"],
     readResources: (args) =>
       plugin.readResources({ ...args, ...env, pluginConfig }),
     writeResources: (args) =>
